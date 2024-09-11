fa4c77d0fcf55e5679385d72e0b58d7e4452d71b
Agnes Applegate <agnesapplegate@agness-air.wireless.newschool.edu>
2024-09-11 15:48:49 -0400

    git commit agnes

---
+++ unit1/lesson3/manovichbot.py
@@ -0,0 +1,12 @@
+import sys
+from manovichdata import *
+for v in variability:
+    print(v)
+    # if everything imported correctly and getting user error in testing "for" loop make sure data is saved first
+
+variability
+
+
+
+
+
---
+++ unit1/lesson3/manovichdata.py
@@ -0,0 +1,6 @@
+variability = ["new media is never really stagnant","is media ever really new if it's always changing", "a new media object is never set to a singular definition" ]
+#using = to prescribe meaning for the object
+automation = ["agents that run automatically exist in low-level examples like Photoshop and high-level examples like AI", "not autonomy", "factory model" ]
+transcoding = ["culture and computer combined","the computerization of culture", "seen as big consequence of new media"]
+modularity = ["fractural structure of system", "independent systems in bigger systems","compartmentalizing system so can change one part without all"]
+numericalrepresentation = [" all new media are composed of digital code","subject to algorithmic manipulation", "numbers behind it all" ]
